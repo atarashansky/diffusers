226e4140b6dd7f7eb9fb9c82e82e505bc6a4abcf
alec <alec>
2022-08-23 23:29:14 +0000

    update

--- src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
+++ src/diffusers/pipelines/stable_diffusion/pipeline_stable_diffusion.py
@@ -43,17 +43,12 @@ class StableDiffusionPipeline(DiffusionPipeline):
         num_inference_steps: Optional[int] = 50,
         guidance_scale: Optional[float] = 7.5,
         eta: Optional[float] = 0.0,
-        seed_x = 42,
-        seed_y = 42,
+        seed = 42,
         n_iter=1,
-        torch_device: Optional[Union[str, torch.device]] = None,
-    ):
+        init_image=None,
+        strength = 0.8
+    ):      
         output_type = "np"
-        
-        
-        if torch_device is None:
-            torch_device = "cuda" if torch.cuda.is_available() else "cpu"
-
 
         if isinstance(prompt, str):
             batch_size = 1
@@ -75,44 +70,53 @@ class StableDiffusionPipeline(DiffusionPipeline):
         )
         text_embeddings = self.text_encoder(text_input.input_ids.to(self.device))[0]
 
-        # here `guidance_scale` is defined analog to the guidance weight `w` of equation (2)
-        # of the Imagen paper: https://arxiv.org/pdf/2205.11487.pdf . `guidance_scale = 1`
-        # corresponds to doing no classifier free guidance.
+        # set timesteps
+        accepts_offset = "offset" in set(inspect.signature(self.scheduler.set_timesteps).parameters.keys())
+        extra_set_kwargs = {}
+        offset=0
+        if accepts_offset:
+            extra_set_kwargs["offset"] = 1  
+            offset=1
+        self.scheduler.set_timesteps(num_inference_steps, **extra_set_kwargs)
+
+        init_latents = None
+        init_timestep = None
+        if init_image is not None:
+            init_image = init_image.resize((width,height))
+            init_latents = self.encode_image(init_image)
+            init_timestep = int(num_inference_steps * strength) + offset
+            init_timestep = min(init_timestep, num_inference_steps)
+            timesteps = self.scheduler.timesteps[-init_timestep]
+            timesteps = torch.tensor([timesteps], dtype=torch.long, device=self.device)
+            # prepare init_latents noise to latents
+        
         do_classifier_free_guidance = guidance_scale > 1.0
-        # get unconditional embeddings for classifier free guidance
         if do_classifier_free_guidance:
             max_length = text_input.input_ids.shape[-1]
             uncond_input = self.tokenizer(
                 [""] * batch_size, padding="max_length", max_length=max_length, return_tensors="pt"
             )
             uncond_embeddings = self.text_encoder(uncond_input.input_ids.to(self.device))[0]
-
-            # For classifier free guidance, we need to do two forward passes.
-            # Here we concatenate the unconditional and text embeddings into a single batch
-            # to avoid doing two forward passes
             text_embeddings = torch.cat([uncond_embeddings, text_embeddings])
 
         # get the intial random noise
         seeds = np.zeros((n_iter,batch_size))
         all_images = []
         all_PIL = []
+        
         for ITER in range(n_iter):
             print("Dreaming iteration",ITER)
             latents = []
             for i in range(batch_size):
-                seed = (seed_x+ITER) * (seed_y+i)
-                seeds[ITER,i] = seed
-                torch.manual_seed(seed)         
-                latents.append(torch.randn(
-                    (self.unet.in_channels, height // 8, width // 8),
-                    device=torch_device,
-                ))
+                seeds[ITER,i] = seed + i*ITER + i
+                torch.manual_seed(seeds[ITER,i])  
+                noise = torch.randn((self.unet.in_channels, height // 8, width // 8), device=self.device)
+                if init_image is not None:
+                    latents.append(self.scheduler.add_noise(init_latents, noise, timesteps)[0])
+                else:    
+                    latents.append(noise)
+            
             latents = torch.stack(latents)
-            # set timesteps
-            accepts_offset = "offset" in set(inspect.signature(self.scheduler.set_timesteps).parameters.keys())
-            extra_set_kwargs = {}
-            if accepts_offset:
-                extra_set_kwargs["offset"] = 1
 
             self.scheduler.set_timesteps(num_inference_steps, **extra_set_kwargs)
 
@@ -128,8 +132,10 @@ class StableDiffusionPipeline(DiffusionPipeline):
             extra_step_kwargs = {}
             if accepts_eta:
                 extra_step_kwargs["eta"] = eta
-
-            for i, t in (enumerate(self.scheduler.timesteps)):
+            
+            
+            t_start = 0 if init_image is None else max(num_inference_steps - init_timestep + offset, 0)
+            for i, t in (enumerate(self.scheduler.timesteps[t_start:])):
                 # expand the latents if we are doing classifier free guidance
                 latent_model_input = torch.cat([latents] * 2) if do_classifier_free_guidance else latents
                 if isinstance(self.scheduler, LMSDiscreteScheduler):
@@ -150,12 +156,7 @@ class StableDiffusionPipeline(DiffusionPipeline):
                 else:
                     latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs)["prev_sample"]
 
-            # scale and decode the image latents with vae
-            latents = 1 / 0.18215 * latents
-            image = self.vae.decode(latents)
-
-            image = (image / 2 + 0.5).clamp(0, 1)
-            image = image.cpu().permute(0, 2, 3, 1).numpy()
+            image = self.decode_image(latents,return_pil=False)
             all_images.append(torch.from_numpy(image))
             all_PIL.append([Image.fromarray((image[i]*255).astype('uint8')) for i in range(batch_size)])
         
@@ -168,3 +169,43 @@ class StableDiffusionPipeline(DiffusionPipeline):
         grid = 255. * rearrange(grid, 'c h w -> h w c').cpu().numpy()
         grid = Image.fromarray(grid.astype(np.uint8))        
         return grid, all_PIL, seeds
+    
+    @torch.no_grad()
+    def encode_image(self,image):
+        numpy_image = np.array(image).astype(np.float32) / 255.0
+        numpy_image = numpy_image[None].transpose(0, 3, 1, 2)
+        numpy_image = torch.from_numpy(numpy_image).to(self.device)
+        encoded = self.vae.encode(2*numpy_image-1)
+        latent_mode = encoded.sample() * 0.18215
+        return latent_mode
+    
+    @torch.no_grad()
+    def decode_image(self,latent,return_pil=True):
+        output_image = self.vae.decode(1 / 0.18215 * latent)
+        output_image = (output_image / 2 + 0.5).clamp(0, 1)
+        output_image = output_image.cpu().permute(0, 2, 3, 1).numpy()
+        if return_pil:
+            output_image = Image.fromarray((output_image[0]*255).astype('uint8'))
+        return output_image        
+    
+    
+    def make_grid(self,
+                  prompt,
+                  seed=42,
+                  num_rows=4,
+                  num_columns=4,
+                  height=512,
+                  width=512,
+                  guidance_scale=7.5,
+                  num_inference_steps=50,
+                  init_image=None,
+                  strength=0.8):
+        return self(num_columns * [prompt],
+                             guidance_scale=guidance_scale,
+                             num_inference_steps=num_inference_steps,
+                             n_iter = num_rows,
+                             height=height,
+                             width=width,
+                             seed=seed,
+                             init_image=init_image,
+                             strength=strength)
